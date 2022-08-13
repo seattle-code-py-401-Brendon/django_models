34ad11570050b5612b5c56b35b5c8eb250de60e0
BrendonLH <Brendonhampton1988@gmail.com>
2022-08-13 13:42:46 -0700

    added some initial base tests

--- snacks/tests.py
+++ snacks/tests.py
@@ -1,3 +1,16 @@
-from django.test import TestCase
+from django.test import SimpleTestCase
+from django.urls import reverse
+
 
 # Create your tests here.
+class Snacks_project_test(SimpleTestCase):
+
+    def test_home_page_status_code(self):
+        url = reverse('home')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_home_page_templates(self):
+        url = reverse('home')
+        response = self.client.get(url)
+        self.assertTemplateUsed(response, 'home.html')
